c9f52284c51f4e5bd12ef1aec1dd8f4a19b37877
iWumbo19 <rancourtvideo@gmail.com>
2021-10-17 01:03:51 -0400

    Now has ability to compile a list of lists

--- UNH_Schedule_Surfer/Program.cs
+++ UNH_Schedule_Surfer/Program.cs
@@ -6,92 +6,36 @@ using Excel = Microsoft.Office.Interop.Excel;
 namespace UNH_Schedule_Surfer
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
             //Initializes Excel Sheet and sets "Summary" Tab to 'wks'
+            Console.WriteLine("Parsing Data");
             string filePath = Path.Combine(Environment.CurrentDirectory, "schedule.xlsx");
             Excel.Application oExcel = new();
             Excel.Workbook WB = oExcel.Workbooks.Open(filePath);
-            Excel.Worksheet wks = (Excel.Worksheet)WB.Worksheets[1];
-            Excel.Range xlRange = wks.UsedRange;
+            XLData.wks = (Excel.Worksheet)WB.Worksheets[1];
+            XLData.xlRange = XLData.wks.UsedRange;
+            XLData.rowCount = XLData.xlRange.Rows.Count;
+            XLData.colCount = XLData.xlRange.Columns.Count;
+            XLData.bldgCodes = XLData.GatherBldgCodes();
+            Console.WriteLine("Building Codes Parsed!");
+            Console.WriteLine($"Attempting to Parse {XLData.bldgCodes.Count} room codes");
+            XLData.roomCodes = XLData.BuildRoomCodes();
+            Console.WriteLine("Room Codes Parsed!");
+            Console.WriteLine("Parsing Complete");
 
-            int rowCount = xlRange.Rows.Count;
-            int colCount = xlRange.Columns.Count;
-            Console.WriteLine($"{rowCount} by {colCount} grid");
-            Console.WriteLine(xlRange[6][5].Value);
-
-
-            List<string> bldgCodes = GatherBldgCodes(rowCount, xlRange);
-            foreach (var item in bldgCodes)
+            Console.WriteLine("Ready for printing");
+            Console.ReadLine();
+            foreach (var item in XLData.roomCodes)
             {
-                Console.Write($"{item}, ");
-            }
-            Console.WriteLine();
-            List<string> roomCodes = GatherRoomCodes(rowCount, xlRange, bldgCodes[3]);
-            foreach (var item in roomCodes)
-            {
-                Console.Write($"{item}, ");
-            }
-            Console.WriteLine();
-            List<string> beginTimes = GatherBeginTimes(rowCount, xlRange, bldgCodes[3], roomCodes[3]);
-            foreach (var item in beginTimes)
-            {
-                Console.Write($"{item}, ");
+                Console.Write(item + ", ");
             }
 
 
             Console.ReadLine();
         }
 
-        static List<string> GatherBldgCodes(int rowCount, dynamic xlRange)
-        {
-            List<string> bldgCodes = new List<string>();
-            for (int i = 2; i < rowCount; i++)
-            {
-                string item = xlRange[2][i].Value;
-                if (!bldgCodes.Contains(item)) { bldgCodes.Add(item); }
-            }
-            return bldgCodes;
-        }
-
-        static List<string> GatherRoomCodes(int rowCount, dynamic xlRange, string bldg)
-        {
-            List<string> roomCodes = new List<string>();
-            for (int i = 2; i < rowCount; i++)
-            {
-                string item = xlRange[4][i].Value;
-                if (!roomCodes.Contains(item) && xlRange[2][i].Value == bldg) { roomCodes.Add(item); }
-            }
-            return roomCodes;
-        }
-
-        static List<string> GatherBeginTimes(int rowCount, dynamic xlRange, string bldg, string room)
-        {
-            List<string> beginTimes = new List<string>();
-            for (int i = 2; i < rowCount; i++)
-            {
-                string item = xlRange[6][i].Value;
-                if (!beginTimes.Contains(item) && 
-                    xlRange[4][i].Value == room && 
-                    xlRange[2][i].Value == bldg) 
-                { beginTimes.Add(item); }
-            }
-            return beginTimes;
-        }
-
-        static List<string> GatherEndTimes(int rowCount, dynamic xlRange,string bldg, string room)
-        {
-            List<string> endTimes = new List<string>();
-            for (int i = 2; i < rowCount; i++)
-            {
-                string item = xlRange[4][i].Value;
-                if (!endTimes.Contains(item) && 
-                    xlRange[4][i].Value == room && 
-                    xlRange[2][i].Value == bldg) 
-                { endTimes.Add(item); }
-            }
-            return endTimes;
-        }
+        
     }
 }
---
+++ UNH_Schedule_Surfer/XLData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace UNH_Schedule_Surfer
+{
+    static public class XLData
+    {
+        static public Excel.Worksheet wks;
+        static public int rowCount;
+        static public int colCount;
+        static public Excel.Range xlRange;
+        static public List<string> bldgCodes;
+        static public List<List<string>> roomCodes;
+
+        static public List<string> GatherBldgCodes()
+        {
+            List<string> bldgCodes = new List<string>();
+            for (int i = 2; i < XLData.rowCount; i++)
+            {
+                string item = XLData.xlRange[2][i].Value;
+                if (!bldgCodes.Contains(item)) { bldgCodes.Add(item); }
+            }
+            return bldgCodes;
+        }
+
+        static public List<string> GatherRoomCodes(string bldg)
+        {
+            List<string> roomCodes = new List<string>();
+            for (int i = 2; i < XLData.rowCount; i++)
+            {
+                string item = XLData.xlRange[4][i].Value;
+                if (XLData.xlRange[2][i].Value == bldg && !roomCodes.Contains(item)) { roomCodes.Add(item); }
+            }
+            return roomCodes;
+        }
+
+        static public List<string> GatherBeginTimes(string bldg, string room)
+        {
+            List<string> beginTimes = new List<string>();
+            for (int i = 2; i < XLData.rowCount; i++)
+            {
+                string item = XLData.xlRange[6][i].Value;
+                if (!beginTimes.Contains(item) &&
+                    XLData.xlRange[4][i].Value == room &&
+                    XLData.xlRange[2][i].Value == bldg)
+                { beginTimes.Add(item); }
+            }
+            return beginTimes;
+        }
+
+        static public List<string> GatherEndTimes(string bldg, string room)
+        {
+            List<string> endTimes = new List<string>();
+            for (int i = 2; i < XLData.rowCount; i++)
+            {
+                string item = XLData.xlRange[4][i].Value;
+                if (!endTimes.Contains(item) &&
+                    XLData.xlRange[4][i].Value == room &&
+                    XLData.xlRange[2][i].Value == bldg)
+                { endTimes.Add(item); }
+            }
+            return endTimes;
+        }
+
+
+        static public List<List<string>> BuildRoomCodes()
+        {
+            List<List<string>> outputList = new List<List<string>>();
+            string currentBldg;
+            List<string> currentRoomList = new List<string>();
+            for (int _code = 0; _code < bldgCodes.Count(); _code++)
+            {
+                currentBldg = bldgCodes[_code];
+                currentRoomList = null;
+                currentRoomList = GatherRoomCodes(currentBldg);
+                outputList.Add(currentRoomList);
+                Console.WriteLine($"Compiled rooms for {currentBldg}");
+            }
+            return outputList;
+        }
+
+
+
+        //static public List<List<string>> BuildRoomCodes()
+        //{
+        //    List<List<string>> outputList;
+        //    List<string> currentList;
+        //    foreach (var item in bldgCodes)
+        //    {
+        //        currentList = null;
+        //        for (int i = 2; i < XLData.rowCount; i++)
+        //        {
+        //            string room = XLData.xlRange[4][i].Value;
+        //            if (XLData.xlRange[2][i].Value == item && !currentList.Contains(item)) { currentList.Add(room); }
+        //        }
+        //        outputList.Add(currentList);
+        //    }
+        //    return outputList;
+        //}
+    }
+}
